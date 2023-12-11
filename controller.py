ff7ba95d02ec557c9d598fc4a1beba5868b92b6c
Rodolphe <rodolphe.andrieux@ynov.com>
2023-12-11 17:08:31 +0100

    slowly progressing

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
-from storage import load_clients, load_rooms, load_reservations
+from models import Room, Client
+from storage import load_clients, load_rooms, load_reservations, save_clients
 
 Clients = load_clients()
 Rooms = load_rooms()
@@ -19,3 +20,57 @@ def show_reservations():
 
 def show_rooms():
     print(Rooms)
+
+
+def show_room(room_number):
+    for room in Rooms:
+        if room['number'] == room_number:
+            print(room)
+            for reservation in Reservations:
+                if reservation['room'] == room_number:
+                    print(f'reservation found: {reservation}')
+            return
+    print(f"No room found with number {room_number}")
+
+
+def add_room(args):
+    room_number, room_type, price_per_night = args
+
+    for room in Rooms:
+        if room['number'] == room_number:
+            print(f"Room {room_number} already exists.")
+            return
+
+    new_room = Room(room_number, room_type, price_per_night)
+    print(f"Room {room_number} added successfully.")
+    Rooms.append(new_room)
+
+
+def add_client(args):
+    client_id = len(Clients) + 1
+    last_name, first_name, birth_date, phone = args
+
+    new_client = Client(client_id, last_name, first_name, birth_date, phone)
+    Clients.append(new_client)
+    print(f"Client {first_name} {last_name} added successfully.")
+    save_clients(Clients)
+
+
+def del_room(args):
+    pass
+
+
+def del_client(args):
+    pass
+
+
+def del_reservation(args):
+    pass
+
+
+def update_room(args):
+    pass
+
+
+def update_client(args):
+    pass
--- ui.py
+++ ui.py
@@ -1,7 +1,9 @@
 import getpass
+import logging
 import re
 from datetime import datetime
-from controller import book, show_rooms, show_clients, show_reservations
+from controller import book, show_rooms, show_clients, show_reservations, add_room, add_client, del_room, del_client, \
+    del_reservation, update_room, update_client, show_room
 
 # globals
 username = getpass.getuser()
@@ -20,9 +22,101 @@ def display_menu():
 
     if Helper == 1:
         Helper = 0
-        return input(f"\n{green}hotel reservation manager{client_color} @{username} {reset}"
+        return input(f"\n{green}hotel booking manager{client_color} @{username} {reset}"
                      f"(try '{input_color}help{reset}')\n$ ")
-    return input(f"\n{green}hotel reservation manager{client_color} @{username} {reset}\n$ ")
+    return input(f"\n{green}hotel booking manager{client_color} @{username} {reset}\n$ ")
+
+
+def validate_room(args):
+    if len(args) != 3:
+        return "Invalid number of arguments. Expected 3 arguments."
+
+    room, room_type, price = args
+
+    if not room.isdigit():
+        return "Invalid room number. Room number should be numeric."
+
+    if not re.match("^[A-Za-z ]*$", room_type):
+        return "Invalid name. Name should only contain letters and spaces."
+
+    if not price.isdigit():
+        return "Invalid room number. Room number should be numeric."
+
+    return "Valid"
+
+
+def validate_client(args):
+    if len(args) != 4:
+        return ("Invalid number of arguments. Expected 5 arguments: [first name] [last name] [birth date] [phone "
+                "number]")
+
+    first_name, last_name, birth_date, phone = args
+
+    if not re.match("^[A-Za-z ]*$", first_name):
+        return "Invalid first name. Name should only contain letters and spaces."
+
+    if not re.match("^[A-Za-z ]*$", last_name):
+        return "Invalid last name. Name should only contain letters and spaces."
+
+    if not validate_phone_number(phone):
+        return "Invalid phone number. It should be a French format"
+
+    if not validate_date(birth_date):
+        return "Invalid birth date. Expected [dd/mm/yyyy]"
+
+    return "Valid"
+
+
+def check_for_update_client(args):
+    pass
+
+
+def check_for_add_room(args):
+    validation = validate_room(args)
+    if validation != 'valid':
+        logging.warning(validation)
+        return
+    add_room(args)
+
+
+def check_for_add_client(args):
+    validation = validate_client(args)
+    if validation != 'valid':
+        logging.warning(validation)
+        return
+    add_client(args)
+
+
+def check_for_del_room(args):
+    pass
+
+
+def check_for_del_client(args):
+    pass
+
+
+def check_for_del_reservation(args):
+    pass
+
+
+def check_for_update_room(args):
+    validation = validate_room(args)
+    if validation != 'valid':
+        logging.warning(validation)
+        return
+    update_room(args)
+
+
+def check_for_show_room(args):
+    if len(args) != 2:
+        return "Invalid number of arguments. Expected 1 arguments: show room [room_number]"
+
+    room = args[1]
+
+    if not room.isdigit():
+        return "Invalid room number. Room number should be numeric."
+
+    show_room(room)
 
 
 def process_command(command):
@@ -34,28 +128,82 @@ def process_command(command):
     if action == 'help':
         # Documentation
         print("\n**************************")
-        print("help - shows documentation")
-        print("book [room] [name] [start_date] - [end_date] - Add a reservation")
+        print("help - shows this documentation")
+        print("book [room] [name] [start_date] [end_date] - Add a reservation")
+        print("show rooms - Display all rooms")
+        print("show clients - Display all clients")
+        print("show reservations - Display all reservations")
+        print("add room [room_number] [room_type] [price] - Add a new room")
+        print("add client [first_name] [last_name] [birth_date] [phone] - Add a new client")
+        print("delete room [room_number] - Remove a room")
+        print("delete client [client_id] - Remove a client")
+        print("delete reservation [reservation_id] - Remove a reservation")
+        print("update room [room_number] [new_room_type] [new_price] - Update room details")
+        print("update client [client_id] [new_phone] - Update client's contact information")
+        print("exit - Exit the program")
         print("**************************\n")
+
     elif action == 'book':
-        if validate_booking_req(args) == 'Valid':
+        validation = validate_booking_req(args)
+        if validation != 'Valid':
+            logging.warning(validation)
+            print("book [room] [name] [start_date (dd/mm/yyyy)] - [end_date (dd/mm/yyyy)]")
+        else:
             room, name, start_date, end_date = args[0], args[1], args[2], args[3]
-            # Appeler la fonction de réservation
             print(f"Booking room {room} for {name} from {start_date} to {end_date}...")
             book(room, name, start_date, end_date)
-        else:
-            print("book [room] [name] [start_date (dd/mm/yyyy)] - [end_date (dd/mm/yyyy)]")
+
     elif action == 'show':
-        if args[0] == 'rooms':
+        if len(args) < 1:
+            print('show + "clients", "rooms" or "reservations"')
+            print('show room [room number]')
+        elif args[0] == 'rooms':
             show_rooms()
+        elif args[0] == 'room':
+            check_for_show_room(args)
         elif args[0] == 'clients':
             show_clients()
         elif args[0] == 'reservations':
             show_reservations()
         else:
             print('show + "clients", "rooms" or "reservations"')
+            print('show room [room number]')
+
+    elif action == 'add':
+        if len(args) < 1:
+            print('add + "client" or "room"')
+        elif args[0] == 'room':
+            check_for_add_room(args)
+        elif args[0] == 'client':
+            check_for_add_client(args)
+        else:
+            print('add + "client" or "room"')
+
+    elif action == 'delete':
+        if len(args) < 1:
+            print('delete + "client", "room" or "reservation"')
+        elif args[0] == 'room':
+            check_for_del_room(args)
+        elif args[0] == 'client':
+            check_for_del_client(args)
+        elif args[0] == 'reservation':
+            check_for_del_reservation(args)
+        else:
+            print('delete + "client", "room" or "reservation"')
+
+    elif action == 'update':
+        if args[0] == 'room':
+            check_for_update_room(args)
+        elif args[0] == 'client':
+            check_for_update_client(args)
+        else:
+            print('add + "client" or "rooms"')
+
     elif action == 'exit':
         return False  # Signal to exit the program
+
+    else:
+        print(f'"{action}" command not found. Try "help" to see the documentation')
     return True
 
 
@@ -67,24 +215,29 @@ def validate_date(date_str):
         return False
 
 
+def validate_phone_number(num):
+    # Regex pour un numéro de téléphone français
+    pattern = re.compile(r'^(?:\+33\s1|\d{2})\s\d{2}\s\d{2}\s\d{2}\s\d{2}$')
+
+    if pattern.match(num):
+        return True
+    else:
+        return False
+
+
 def validate_booking_req(args):
     if len(args) != 4:
         return "Invalid number of arguments. Expected 4 arguments."
 
     room, name, start_date, end_date = args
 
-    # Vérifier si le numéro de chambre est un nombre
     if not room.isdigit():
         return "Invalid room number. Room number should be numeric."
 
-    # Vérifier le format des noms (pas de caractères spéciaux)
     if not re.match("^[A-Za-z ]*$", name):
         return "Invalid name. Name should only contain letters and spaces."
 
-    # Valider les dates
     if not validate_date(start_date) or not validate_date(end_date):
         return "Invalid date format. Use DD/MM/YYYY."
-
-    # Vous pouvez ajouter d'autres validations ici si nécessaire
 
     return "Valid"
